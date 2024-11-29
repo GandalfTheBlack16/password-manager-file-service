0e4e00413b834a51d9a6ff7bf3bf64897f070bb1
GandalfTheBlack16 <juanitonavarro90@gmail.com>
2024-11-29 20:11:30 +0100

    Created Spring boot scaffolding

---
+++ src/main/java/com/gandalftheblack/pm/fileservice/PasswordManagerFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gandalftheblack.pm.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordManagerFileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gandalftheblack/pm/fileservice/PasswordManagerFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gandalftheblack.pm.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
